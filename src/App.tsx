c969edb81d2d1c940329095d1f07d72ef034938a
김내림 <naerim1119@naver.com>
2024-02-07 09:51:16 +0900

    prettier 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 import ChattingContainer from "./container/ChattingContainer";
 import ChattingRoomContainer from "./components/Chat/ChattingRoomContainer";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const AuthRoutes = () => (
-    <Routes>
-        <Route path="/" element={<ChattingContainer/>}/>
-      <Route path="/chat" element={<ChattingContainer/>}/>
-      <Route path="/chat/:no" element={<ChattingRoomContainer/>}/>
-    </Routes>
+  <Routes>
+    <Route path="/" element={<ChattingContainer />} />
+    <Route path="/chat" element={<ChattingContainer />} />
+    <Route path="/chat/:no" element={<ChattingRoomContainer />} />
+  </Routes>
 );
 
 function App() {
   return (
-      <BrowserRouter>
-        <AuthRoutes/>
-      </BrowserRouter>
+    <BrowserRouter>
+      <AuthRoutes />
+    </BrowserRouter>
   );
 }
 
