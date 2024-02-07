af6257cb665bf078b63d90ec7e12d2c7ef4f2f8b
김내림 <naerim1119@naver.com>
2024-02-07 10:08:40 +0900

    [FEAT] font, global style setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import "./App.css";
-import ChattingContainer from "./container/ChattingContainer";
-import ChattingRoomContainer from "./components/Chat/ChattingRoomContainer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainContainer from "./container/MainContainer";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./style/theme.";
+import { GlobalStyle } from "./style/GlobalStyles";
 
 const AuthRoutes = () => (
   <Routes>
     <Route path="/" element={<MainContainer />} />
-    <Route path="/chat" element={<ChattingContainer />} />
-    <Route path="/chat/:no" element={<ChattingRoomContainer />} />
+    {/*<Route path="/chat" element={<ChattingContainer />} />*/}
+    {/*<Route path="/chat/:no" element={<ChattingRoomContainer />} />*/}
   </Routes>
 );
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <AuthRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
